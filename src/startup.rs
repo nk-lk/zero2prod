fdec997bf8b8ec5c4b5ef6797a9ca16e81d548e8
nk-lk <qxnk451@gmail.com>
2024-04-16 16:52:17 +0300

    added logging

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 
@@ -10,6 +11,7 @@ pub fn run(listener: TcpListener, connection_pool: PgPool) -> Result<Server, std
     let connection = web::Data::new(connection_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(connection.clone())
